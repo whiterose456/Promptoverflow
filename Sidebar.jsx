716c3be67d4d596ac2ee807dd5635555cf8acef0
Whiterose223 <136229850+whiterose456@users.noreply.github.com>
2026-05-14 11:56:41 +0700

    Add files via upload

---
+++ Sidebar.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Sidebar = () => {
+  return (
+    <aside className="sidebar">
+      <div className="sidebar-content">
+        {/* Add sidebar content */}
+      </div>
+    </aside>
+  );
+};
+
+export default Sidebar;
